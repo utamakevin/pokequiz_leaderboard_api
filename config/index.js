aa28202b8ef2aaa1a776e4b4c694fbfe757eec5e
Amingman <stefanuslie91@gmail.com>
2023-04-24 09:59:46 +0700

    enable cors and db url for production

--- config/index.js
+++ config/index.js
@@ -8,9 +8,23 @@ if (missing.length > 0) {
   throw Error(`missing environment variables for ${missing.join(", ")}`)
 }
 
-module.exports = {
-  port: process.env.PORT,
-  db: {
+const deployment = {
+  prod: {
+    connectionString:
+      "postgres://pokequiz_user:hq0k9ErHGq2gL1fbbRi1sWBiWQTSwbmX@dpg-ch2uevt269v61ffhh5dg-a/pokequiz",
+  },
+  dev: {
     connectionString: process.env.DATABASE_URL,
   },
+}
+let link
+if (!process.env.DATABASE_URL) {
+  link = deployment.prod
+} else {
+  link = deployment.dev
+}
+
+module.exports = {
+  port: process.env.PORT,
+  db: link,
 }
